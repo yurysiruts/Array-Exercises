11703ad0b400df7f86538b8583d36b8e754b15bb
yurysiruts <sirut.2003@gmail.com>
2021-04-03 01:33:09 +0200

    version 1.0.4

--- main.js
+++ main.js
@@ -43,3 +43,49 @@ function setResult(msg, color) {
    result.textContent = msg;
    result.style.color = color;
 }
+
+
+
+// ---------------------- Task no.2 ---------------------- //
+// lata przestępne
+
+arr = [];
+yearsArray = [];
+
+document.getElementById('submit-btn').addEventListener('click', function getNumbers() {
+   const rok1 = document.getElementById('firstYear').value;
+   const rok2 = document.getElementById('secondYear').value ;
+   
+   console.log(Number(rok1), Number(rok2));
+   if (rok1 < rok2) {
+      for (let i = Number(rok1); i >= rok1 && i <= rok2; i++) {
+         arr.push(i)
+      };
+   } else {
+      console.log('First year was bigger then the second one!')
+   }
+   
+   const check = { x: 4, y: 100, z: 400 };
+   arr.forEach( function (element) {
+      const ostatok1 = element % check.x;
+      const ostatok2 = element % check.y;
+      const ostatok3 = element % check.z;
+
+      if (ostatok1 == 0) {
+         if (ostatok2 == 0) {
+            if (ostatok3 == 0) {
+               console.log(`${element} - Год високосный!`);
+               yearsArray.push(element);
+            } else {
+            }
+         } else {
+            console.log(`${element} - Год високосный!`);
+            yearsArray.push(element);
+         }
+      } else {
+      }
+   })
+   console.log(yearsArray);
+})
+
+
