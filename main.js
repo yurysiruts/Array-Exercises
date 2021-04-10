1b703919f09493a454532a81dd2fe2ec043ff261
yurysiruts <sirut.2003@gmail.com>
2021-04-10 02:59:15 +0200

    version 1.0.5

--- main.js
+++ main.js
@@ -49,43 +49,68 @@ function setResult(msg, color) {
 // ---------------------- Task no.2 ---------------------- //
 // lata przestępne
 
-arr = [];
-yearsArray = [];
+// arr = [];
+// yearsArray = [];
 
-document.getElementById('submit-btn').addEventListener('click', function getNumbers() {
-   const rok1 = document.getElementById('firstYear').value;
-   const rok2 = document.getElementById('secondYear').value ;
+// document.getElementById('submit-btn').addEventListener('click', function getNumbers() {
+//    const rok1 = document.getElementById('firstYear').value;
+//    const rok2 = document.getElementById('secondYear').value ;
    
-   console.log(Number(rok1), Number(rok2));
-   if (rok1 < rok2) {
-      for (let i = Number(rok1); i >= rok1 && i <= rok2; i++) {
-         arr.push(i)
-      };
-   } else {
-      console.log('First year was bigger then the second one!')
-   }
+//    console.log(Number(rok1), Number(rok2));
+//    if (rok1 < rok2) {
+//       for (let i = Number(rok1); i >= rok1 && i <= rok2; i++) {
+//          arr.push(i)
+//       };
+//    } else {
+//       console.log('First year was bigger then the second one!')
+//    }
    
-   const check = { x: 4, y: 100, z: 400 };
-   arr.forEach( function (element) {
-      const ostatok1 = element % check.x;
-      const ostatok2 = element % check.y;
-      const ostatok3 = element % check.z;
+//    const check = { x: 4, y: 100, z: 400 };
+//    arr.forEach( function (element) {
+//       const ostatok1 = element % check.x;
+//       const ostatok2 = element % check.y;
+//       const ostatok3 = element % check.z;
 
-      if (ostatok1 == 0) {
-         if (ostatok2 == 0) {
-            if (ostatok3 == 0) {
-               console.log(`${element} - Год високосный!`);
-               yearsArray.push(element);
-            } else {
-            }
-         } else {
-            console.log(`${element} - Год високосный!`);
-            yearsArray.push(element);
-         }
-      } else {
+//       if (ostatok1 == 0) {
+//          if (ostatok2 == 0) {
+//             if (ostatok3 == 0) {
+//                console.log(`${element} - Год високосный!`);
+//                yearsArray.push(element);
+//             } else {
+//             }
+//          } else {
+//             console.log(`${element} - Год високосный!`);
+//             yearsArray.push(element);
+//          }
+//       } else {
+//       }
+//    })
+//    console.log(yearsArray);
+// })
+
+
+// ---------------------- Task no.3 ---------------------- //
+
+document.querySelector('.btn').addEventListener('click', function() {
+   let sum = 0;
+   let strNumber = 0;
+   let number = 0;
+   while( strNumber != null) {
+      strNumber = prompt("Type in a number:", ""); 
+      if (strNumber == '') {
+         break
       }
-   })
-   console.log(yearsArray);
+      number = +strNumber;
+      sum += number; 
+   }
+   if(isNaN != true) {
+      alert( 'Suma wszystkich wprowadzonych liczb wynosi: ' + sum);
+   }
 })
+
+
+
+// ---------------------- Task no.4 Zamena na camelCase  ---------------------- //
+
 
 
